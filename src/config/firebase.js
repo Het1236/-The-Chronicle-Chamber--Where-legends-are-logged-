d32df48fdf23e584897b901f0ea4324ca95c6e18
Het1236 <1236hetpatel@gmail.com>
2025-07-18 23:39:06 +0530

    safety

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,31 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_DMljZ-JzYADkgN8O5rs_xk5hHOc1n4A",
-  authDomain: "the-chronicle-chamber.firebaseapp.com",
-  projectId: "the-chronicle-chamber",
-  storageBucket: "the-chronicle-chamber.firebasestorage.app",
-  messagingSenderId: "913451149132",
-  appId: "1:913451149132:web:2264ae507b2bf118fad1e9",
-  measurementId: "G-T9XKEK5Q2G"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA_DMljZ-JzYADkgN8O5rs_xk5hHOc1n4A",
+//   authDomain: "the-chronicle-chamber.firebaseapp.com",
+//   projectId: "the-chronicle-chamber",
+//   storageBucket: "the-chronicle-chamber.firebasestorage.app",
+//   messagingSenderId: "913451149132",
+//   appId: "1:913451149132:web:2264ae507b2bf118fad1e9",
+//   measurementId: "G-T9XKEK5Q2G"
+// };
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
